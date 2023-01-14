914b313cd7ed193c477b6cec2d463208c3cfdc8b
Aaron Santiago <me@aaron.work>
2023-01-14 14:04:42 -0500

    Update bubbleOrbit

--- Assets/Scripts/BubbleOrbit.cs
+++ Assets/Scripts/BubbleOrbit.cs
@@ -5,15 +5,36 @@ using UnityEngine;
 public class BubbleOrbit : MonoBehaviour
 {
     public Transform orbitTransform;
+    public float amplitude = 1f;
+    public float frequency = 1f;
+    public float phase = 0f;
+
+    public float yAmplitude = 1f;
+    public float yFrequency = 1f;
+    public float yPhase = 0f;
+
+    //public float ySpeed = 1f;
+
+    //public float maxY = 1f;
+    //public float minY = -1f;
     // Start is called before the first frame update
     void Start()
     {
-        orbitTransform = PlayerHead.Instance.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(
+            Mathf.Sin(Time.time * frequency + phase * Mathf.PI * 2) * amplitude + orbitTransform.position.x,
+            Mathf.Sin(Time.time * yFrequency + yPhase * Mathf.PI * 2) * yAmplitude + orbitTransform.position.y,
+            Mathf.Cos(Time.time * frequency + phase * Mathf.PI * 2) * amplitude + orbitTransform.position.z
+          );
+
+        //if (transform.position.y > orbitTransform.position.y + maxY)
+        //{
+        //    transform.position = new Vector3(transform.position.x, orbitTransform.position.y + minY, transform.position.z);
+        //}
+
     }
 }
